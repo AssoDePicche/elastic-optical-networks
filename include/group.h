f5be5b500b0dea81aa006a6cac0814869cd011f1
Samuel do Prado Rodrigues <samuelprado730@gmail.com>
2024-07-24 15:02:31 -0300

    feat: group

---
+++ include/group.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <initializer_list>
+#include <tuple>
+
+#include "distribution.h"
+
+class Group final {
+public:
+  Group(const std::initializer_list<double> &,
+        const std::initializer_list<std::size_t> &);
+
+  auto count_blocking(const std::size_t) -> void;
+
+  [[nodiscard]] auto size(void) const -> std::size_t;
+
+  [[nodiscard]] auto blocked(void) const -> std::size_t;
+
+  [[nodiscard]] auto next(void) -> std::size_t;
+
+  [[nodiscard]] auto to_string(void) const -> std::string;
+
+private:
+  Discrete roulette;
+  std::vector<std::tuple<std::size_t, std::size_t, std::size_t>> container;
+  std::size_t _size;
+  std::size_t _blocking;
+};
---
+++ source/group.cpp
@@ -0,0 +1,69 @@
+#include "group.h"
+
+#include <cassert>
+
+Group::Group(const std::initializer_list<double> &predicted,
+             const std::initializer_list<std::size_t> &resources)
+    : roulette{predicted} {
+  assert(predicted.size() == resources.size());
+
+  for (auto index{0u}; index < predicted.size(); ++index) {
+    const auto request{*(resources.begin() + index)};
+
+    const auto value{std::make_tuple(request, 0, 0)};
+
+    container.push_back(value);
+  }
+}
+
+auto Group::count_blocking(const std::size_t request) -> void {
+  for (auto &[resource, counting, blocking] : container) {
+    if (resource != request) {
+      continue;
+    }
+
+    ++blocking;
+
+    ++_blocking;
+
+    break;
+  }
+}
+
+auto Group::size(void) const -> std::size_t { return _size; }
+
+auto Group::blocked(void) const -> std::size_t { return _blocking; }
+
+auto Group::next(void) -> std::size_t {
+  const auto index{static_cast<std::size_t>(roulette.next())};
+
+  auto &[resource, counting, blocking] = container[index];
+
+  ++counting;
+
+  ++_size;
+
+  return resource;
+}
+
+auto Group::to_string(void) const -> std::string {
+  std::string buffer{};
+
+  for (const auto &[resource, counting, blocking] : container) {
+    std::string line{};
+
+    line.append("Type " + std::to_string(resource) + "\n");
+
+    const auto ratio{static_cast<double>(counting) / size()};
+
+    line.append("Ratio (%): " + std::to_string(ratio) + "\n");
+
+    const auto bp{static_cast<double>(blocking) / size()};
+
+    line.append("BP (%): " + std::to_string(bp) + "\n");
+
+    buffer.append(line);
+  }
+
+  return buffer;
+}
