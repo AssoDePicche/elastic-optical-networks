7b30a4c5dc8a094ba9ed48b50f44054623fe20e0
AssoDePicche <samuelprado730@gmail.com>
2025-01-02 14:41:19 -0300

    style: code formatting

--- include/logger.h
+++ include/logger.h
@@ -5,28 +5,28 @@
 #include <cstring>
 #include <iostream>
 
-#define __FILENAME__                                                           \
+#define __FILENAME__ \
   (std::strrchr(__FILE__, '/') ? std::strrchr(__FILE__, '/') + 1 : __FILE__)
 
-#define LOG(LEVEL, STR)                                                        \
-  do {                                                                         \
-    std::clog << '[' << LEVEL << "] " << __FILENAME__ << '@' << __func__       \
-              << ": " << STR << std::endl;                                     \
+#define LOG(LEVEL, STR)                                                  \
+  do {                                                                   \
+    std::clog << '[' << LEVEL << "] " << __FILENAME__ << '@' << __func__ \
+              << ": " << STR << std::endl;                               \
   } while (0);
 
-#define ERROR(STR)                                                             \
-  do {                                                                         \
-    LOG("ERROR", STR)                                                          \
+#define ERROR(STR)    \
+  do {                \
+    LOG("ERROR", STR) \
   } while (0);
 
-#define INFO(STR)                                                              \
-  do {                                                                         \
-    LOG("INFO", STR);                                                          \
+#define INFO(STR)     \
+  do {                \
+    LOG("INFO", STR); \
   } while (0);
 
-#define WARNING(STR)                                                           \
-  do {                                                                         \
-    LOG("WARN", STR);                                                          \
+#define WARNING(STR)  \
+  do {                \
+    LOG("WARN", STR); \
   } while (0);
 #else
 
