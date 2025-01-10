0bef7d1bcfcda1a59eed12fa15adedf0b8ca1ec4
AssoDePicche <samuelprado730@gmail.com>
2025-01-10 11:55:28 -0300

    build: add gtest command

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mkdir -p third_party && cmake -DCMAKE_BUILD_TYPE=Release -S . -B build && cmake --build build -j12
+mkdir -p third_party && cmake -DCMAKE_BUILD_TYPE=Release -S . -B build && cmake --build build -j12 && ctest -j12 --output-on-failure --test-dir build
