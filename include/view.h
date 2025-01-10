96556611697013d3c3096a9edb83b0978408dd0a
AssoDePicche <samuelprado730@gmail.com>
2025-01-10 17:14:18 -0300

    feat: bounding box

--- include/view.h
+++ include/view.h
@@ -7,21 +7,29 @@ namespace raylib {
 #include <raylib.h>
 };
 
+using Color = unsigned int;
+
 using Point2D = std::pair<float, float>;
 
 [[nodiscard]] auto euclidean_distance(const Point2D&, const Point2D&) -> float;
+
+using BoundingBox = std::tuple<Point2D, float, float, Color>;
+
+[[nodiscard]] auto collide(const BoundingBox&, const BoundingBox&) -> bool;
 
 struct Object;
 
 using draw_fn = std::function<void(const Object&)>;
 
 struct Object final {
+  BoundingBox box;
   Point2D point;
+  Color color;
   draw_fn fn;
 
   Object(void) = default;
 
-  Object(const Point2D&, const draw_fn&);
+  Object(const BoundingBox&, const Point2D&, const Color&, const draw_fn&);
 
   auto draw(void) const -> void;
 };
--- source/view.cpp
+++ source/view.cpp
@@ -14,7 +14,24 @@ auto euclidean_distance(const Point2D& p, const Point2D& q) -> float {
   return std::sqrt(dx * dx + dy * dy);
 }
 
-Object::Object(const Point2D& point, const draw_fn& fn)
-    : point{point}, fn{fn} {}
+auto collide(const BoundingBox& a, const BoundingBox& b) -> bool {
+  const auto& [p1, w1, h1, c1] = a;
+
+  const auto& [p2, w2, h2, c2] = b;
+
+  const auto& [x1, y1] = p1;
+
+  const auto& [x2, y2] = p2;
+
+  const auto x_overlap = x1 < x2 + w2 && x1 + w1 > x2;
+
+  const auto y_overlap = y1 < y2 + h2 && y1 + h1 > y2;
+
+  return x_overlap && y_overlap;
+}
+
+Object::Object(const BoundingBox& box, const Point2D& point, const Color& color,
+               const draw_fn& fn)
+    : box{box}, point{point}, color{color}, fn{fn} {}
 
 auto Object::draw(void) const -> void { fn(*this); }
