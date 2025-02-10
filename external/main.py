efd5d4f56ec6d2705c3cb71b19194049551a8949
AssoDePicche <samuelprado730@gmail.com>
2025-02-10 10:15:43 -0300

    feat: data analysis and plotting

--- external/main.py
+++ external/main.py
@@ -1,7 +1,10 @@
+import matplotlib.pyplot as pyplot
 import numpy
 import pandas
+import seaborn
 from imblearn.over_sampling import SMOTE
 from pandas import DataFrame
+from sklearn.cluster import KMeans
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.metrics import (classification_report, f1_score,
                              matthews_corrcoef, roc_auc_score)
@@ -15,13 +18,88 @@ if __name__ == "__main__":
     # (7.51 + 14.68 + 29.36) E | 10 bandwidth | Seed 0
     dataframe: DataFrame = pandas.concat(
         [
-            pandas.read_csv("dataset0.146997.csv"),
-            pandas.read_csv("dataset0.256196.csv"),
-            pandas.read_csv("dataset0.418617.csv"),
+            pandas.read_csv("dataset.csv"),
         ]
     )
 
     dataframe.reset_index(inplace=True, drop=True)
+
+    dataframe["mean_fragmentation"] = dataframe[
+        ["from_1_to_0_fragmentation", "from_0_to_1_fragmentation"]
+    ].mean(axis=1)
+
+    dataframe["mean_entropy"] = dataframe[
+        ["from_1_to_0_entropy", "from_0_to_1_entropy"]
+    ].mean(axis=1)
+
+    print(dataframe.head())
+
+    print(dataframe.describe())
+
+    print(dataframe.isnull().sum())
+
+    print(dataframe.dtypes)
+
+    histogram = dataframe.hist(figsize=(10, 10))
+
+    correlation = dataframe.corr()
+
+    pyplot.figure(figsize=(12, 8))
+
+    seaborn.heatmap(correlation, annot=True, cmap="coolwarm", fmt=".2f")
+
+    pyplot.title("Correlação")
+
+    pyplot.show()
+
+    for column in ["mean_entropy", "mean_fragmentation"]:
+        pearson_correlation = dataframe["blocking"].corr(dataframe[column])
+
+        print(
+            f"Correlação de Pearson (blocking,{column}): {pearson_correlation}"
+        )
+
+    print(dataframe["accepted"].value_counts())
+
+    seaborn.countplot(x="accepted", data=dataframe)
+
+    pyplot.title("Distribuição de requisições")
+
+    pyplot.show()
+
+    seaborn.boxplot(x="accepted", y="blocking", data=dataframe)
+
+    pyplot.title("Boxplot accepted x blocking")
+
+    pyplot.show()
+
+    seaborn.boxplot(x=dataframe["blocking"])
+
+    pyplot.title("Boxplot blocking")
+
+    pyplot.show()
+
+    stratification = [
+        dataframe[
+            (dataframe["accepted"] == True) & (dataframe["bandwidth"] == 3)
+        ],
+        dataframe[
+            (dataframe["accepted"] == True) & (dataframe["bandwidth"] == 5)
+        ],
+        dataframe[
+            (dataframe["accepted"] == False) & (dataframe["bandwidth"] == 3)
+        ],
+        dataframe[
+            (dataframe["accepted"] == False) & (dataframe["bandwidth"] == 5)
+        ],
+    ]
+
+    for data in stratification:
+        pyplot.figure(figsize=(8, 6))
+
+        seaborn.heatmap(
+            data.corr(), annot=True, cmap="coolwarm", fmt=".2f", cbar=True
+        )
 
     x, y = SMOTE(random_state=RANDOM_STATE).fit_resample(
         dataframe.drop(columns=["accepted"]), dataframe["accepted"]
@@ -30,6 +108,8 @@ if __name__ == "__main__":
     x = StandardScaler().fit_transform(x)
 
     dataframe["accepted"] = LabelEncoder().fit_transform(dataframe["accepted"])
+
+    print(dataframe.head())
 
     kfold: KFold = KFold(n_splits=10, shuffle=True, random_state=RANDOM_STATE)
 
