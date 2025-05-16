8e4cd61cb3f705c81f0098c428df807f1b6a2db6
AssoDePicche <samuelprado730@gmail.com>
2025-05-16 16:41:28 -0300

    feat: update logger to format api

--- include/logger.h
+++ include/logger.h
@@ -3,16 +3,17 @@
 #ifdef ALLOWLOGS
 
 #include <cstring>
+#include <format>
 #include <iostream>
 
 #define __FILENAME__ \
   (std::strrchr(__FILE__, '/') ? std::strrchr(__FILE__, '/') + 1 : __FILE__)
 
-#define LOG(LEVEL, STR)                                                  \
-  do {                                                                   \
-    std::clog << '[' << LEVEL << "] " << __FILENAME__ << '@' << __func__ \
-              << ": " << STR << std::endl;                               \
-  } while (0);
+#define LOG(LEVEL, STR)                                               \
+  do {                                                                \
+    std::clog << std::format("[{}] {}@{}: {}\n", LEVEL, __FILENAME__, \
+                             __func__, STR);                          \
+  } while (0)
 
 #define ERROR(STR)    \
   do {                \
