ad19c39582ca2071a51bc94f24da2f81d4ccd7f2
AssoDePicche <samuelprado730@gmail.com>
2025-05-18 22:00:33 -0300

    refactor: move statistics calc to math header

--- include/math.h
+++ include/math.h
@@ -1,7 +1,26 @@
 #pragma once
 
+#include <cmath>
 #include <functional>
+#include <iterator>
+#include <numeric>
 
 using PairingFunction = std::function<unsigned(unsigned, unsigned)>;
 
 [[nodiscard]] unsigned CantorPairingFunction(unsigned, unsigned);
+
+template <class Iterator>
+[[nodiscard]] double Mean(Iterator first, Iterator last) {
+  return std::accumulate(first, last, 0.0) / std::distance(first, last);
+}
+
+template <class Iterator>
+[[nodiscard]] double StandardDeviation(Iterator first, Iterator last) {
+  const auto mean = Mean(first, last);
+
+  const auto variance = std::accumulate(
+      first, last, 0.0,
+      [mean](double sum, double x) { return sum + (x - mean) * (x - mean); });
+
+  return std::sqrt(variance);
+}
