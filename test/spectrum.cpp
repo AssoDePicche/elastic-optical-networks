dbcacfeb0ccd7bad747b9e71d7cb9eda5b3a9a24
AssoDePicche <samuelprado730@gmail.com>
2025-07-16 00:44:16 -0300

    test: best fit, last fit, worst fit

--- test/spectrum.cpp
+++ test/spectrum.cpp
@@ -4,21 +4,64 @@
 
 #include <vector>
 
-TEST(Spectrum, FirstFit) {
+TEST(Spectrum, BestFit) {
   Spectrum spectrum(10);
 
-  std::vector<Slice> slices = {
-      {0, 3},
-      {5, 7},
-  };
+  spectrum.allocate({0, 1});
 
-  for (const auto& slice : slices) {
-    spectrum.allocate(slice);
-  }
+  spectrum.allocate({5, 7});
 
   const Slice expected = {8, 9};
 
+  const auto maybe = BestFit(spectrum, 2);
+
+  ASSERT_TRUE(maybe.has_value());
+
+  ASSERT_EQ(maybe.value(), expected);
+}
+
+TEST(Spectrum, FirstFit) {
+  Spectrum spectrum(10);
+
+  spectrum.allocate({0, 1});
+
+  spectrum.allocate({5, 7});
+
+  const Slice expected = {2, 3};
+
   const auto maybe = FirstFit(spectrum, 2);
+
+  ASSERT_TRUE(maybe.has_value());
+
+  ASSERT_EQ(maybe.value(), expected);
+}
+
+TEST(Spectrum, LastFit) {
+  Spectrum spectrum(10);
+
+  spectrum.allocate({0, 1});
+
+  spectrum.allocate({5, 7});
+
+  const Slice expected = {8, 9};
+
+  const auto maybe = LastFit(spectrum, 2);
+
+  ASSERT_TRUE(maybe.has_value());
+
+  ASSERT_EQ(maybe.value(), expected);
+}
+
+TEST(Spectrum, WorstFit) {
+  Spectrum spectrum(10);
+
+  spectrum.allocate({0, 1});
+
+  spectrum.allocate({5, 7});
+
+  const Slice expected = {2, 3};
+
+  const auto maybe = WorstFit(spectrum, 2);
 
   ASSERT_TRUE(maybe.has_value());
 
