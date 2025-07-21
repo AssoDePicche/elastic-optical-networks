b710f938e34e8144f9b92638b3ddc94ffb36f614
AssoDePicche <samuelprado730@gmail.com>
2025-07-21 10:29:27 -0300

    build: add submodule cloning

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [[ -e "./vendor/googletest/CMakeLists.txt" && -e "./vendor/json/CMakeLists.txt" ]]; then
+  echo "Building..."
+else
+  echo "Cloning submodules" && git submodule update --init --recursive
+fi
+
 cmake -DCMAKE_BUILD_TYPE=Release -S . -B build && cmake --build build -j12
