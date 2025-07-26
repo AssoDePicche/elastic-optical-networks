8bc8a2f44fb971efb94fac83cb3197932655630d
AssoDePicche <samuelprado730@gmail.com>
2025-07-25 23:11:00 -0300

    feat: simulation iterations

--- application/main.cpp
+++ application/main.cpp
@@ -22,9 +22,11 @@ int main(void) {
 
     Settings settings = Settings::From(json).value();
 
+    std::random_device random_device;
+
     const auto prng = PseudoRandomNumberGenerator::Instance();
 
-    prng->set_seed(settings.seed);
+    prng->set_seed(random_device());
 
     prng->set_exponential("arrival", settings.arrivalRate);
 
@@ -36,94 +38,110 @@ int main(void) {
 
     Simulation simulation(settings);
 
-    const auto start = std::chrono::system_clock::now();
+    for (auto iteration = 1u; iteration <= settings.iterations; ++iteration) {
+      const auto start = std::chrono::system_clock::now();
 
-    while (simulation.HasNext()) {
-      simulation.Next();
-    }
-
-    const auto end = std::chrono::system_clock::now();
-
-    const auto execution_time =
-        std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
-
-    const auto snapshots = simulation.GetSnapshots();
-
-    if (settings.exportDataset) {
-      std::string buffer{"time,fsus,accepted,"};
-
-      for (const auto &strategy : settings.fragmentationStrategies) {
-        buffer.append(std::format("{},", strategy.first));
+      while (simulation.HasNext()) {
+        simulation.Next();
       }
 
-      buffer.append("blocking\n");
+      const auto end = std::chrono::system_clock::now();
 
-      std::for_each(snapshots.begin(), snapshots.end(),
-                    [&buffer](const Snapshot &snapshot) {
-                      buffer.append(std::format("{}\n", snapshot.Serialize()));
-                    });
+      const auto execution_time =
+          std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
 
-      const auto datetime = DateTime::now();
+      const auto snapshots = simulation.GetSnapshots();
 
-      const std::string filename = std::format(
-          "{:02}_{:02}_{:04}_{:02}h{:02}.csv", datetime.day, datetime.month,
-          datetime.year, datetime.hour, datetime.minute);
+      if (settings.exportDataset) {
+        std::string buffer{"time,fsus,accepted,"};
 
-      std::ofstream stream(filename);
+        for (const auto &strategy : settings.fragmentationStrategies) {
+          buffer.append(std::format("{},", strategy.first));
+        }
 
-      if (!stream.is_open()) {
-        throw std::runtime_error(
-            std::format("Failed to write {} file", filename));
+        buffer.append("blocking\n");
+
+        std::for_each(
+            snapshots.begin(), snapshots.end(),
+            [&buffer](const Snapshot &snapshot) {
+              buffer.append(std::format("{}\n", snapshot.Serialize()));
+            });
+
+        const auto datetime = DateTime::now();
+
+        const std::string filename = std::format(
+            "resources/temp/{}_iteration_{:02}_{:02}_{:04}_{:02}h{:02}.csv",
+            iteration, datetime.day, datetime.month, datetime.year,
+            datetime.hour, datetime.minute);
+
+        std::ofstream stream(filename);
+
+        if (!stream.is_open()) {
+          throw std::runtime_error(
+              std::format("Failed to write {} file", filename));
+        }
+
+        stream << buffer;
+
+        stream.close();
       }
 
-      stream << buffer;
+      const auto time = simulation.GetTime();
 
-      stream.close();
+      const auto requestCount = simulation.GetRequestCount();
+
+      Document document;
+
+      document.append("iteration: {}\n", iteration)
+          .append("seed: {}\n", prng->get_seed())
+          .append("execution time (s): {}\n", execution_time)
+          .append("simulated time: {:.3f}\n", time)
+          .append("spectrum width (GHz): {:.2f}\n", settings.spectrumWidth)
+          .append("slot width (GHz): {:.2f}\n", settings.slotWidth)
+          .append("fsus per link: {}\n", settings.FSUsPerLink);
+
+      const double load = settings.arrivalRate / settings.serviceRate;
+
+      document.append("load (E): {:.3f}\n", load)
+          .append("arrival rate: {:.3f}\n", settings.arrivalRate)
+          .append("service rate: {:.3f}\n", settings.serviceRate)
+          .append("grade of service: {:.3f}\n", simulation.GetGradeOfService())
+          .append("total requests: {}\n", requestCount);
+
+      for (const auto &request : settings.requests) {
+        const auto ratio =
+            request.second.counting / simulation.GetRequestCount();
+
+        const auto gos = request.second.blocking / simulation.GetRequestCount();
+
+        const auto normalized_load =
+            settings.arrivalRate *
+            (static_cast<double>(request.second.FSUs) / settings.FSUsPerLink);
+
+        document.append("requests for {} FSU(s)\n", request.second.FSUs)
+            .append("ratio: {:.3f}\n", ratio)
+            .append("grade of service: {:.3f}\n", gos)
+            .append("normalized load: {:.3f}\n", normalized_load);
+      }
+
+      const std::string report_filename =
+          std::format("resources/temp/{}_iteration_report.txt", iteration);
+
+      document.write(report_filename);
+
+      std::cout << std::format("Simulation results wrote in {}\n",
+                               report_filename);
+
+      simulation.Reset();
+
+      prng->set_seed(random_device());
+
+      for (auto &request : settings.requests) {
+        request.second.blocking = 0;
+
+        request.second.counting = 0;
+      }
     }
-
-    const auto time = simulation.GetTime();
-
-    const auto requestCount = simulation.GetRequestCount();
-
-    Document document;
-
-    document.append("seed: {}\n", settings.seed)
-        .append("execution time (s): {}\n", execution_time)
-        .append("simulated time: {:.3f}\n", time)
-        .append("spectrum width (GHz): {:.2f}\n", settings.spectrumWidth)
-        .append("slot width (GHz): {:.2f}\n", settings.slotWidth)
-        .append("fsus per link: {}\n", settings.FSUsPerLink);
-
-    const double load = settings.arrivalRate / settings.serviceRate;
-
-    document.append("load (E): {:.3f}\n", load)
-        .append("arrival rate: {:.3f}\n", settings.arrivalRate)
-        .append("service rate: {:.3f}\n", settings.serviceRate)
-        .append("grade of service: {:.3f}\n", simulation.GetGradeOfService())
-        .append("total requests: {}\n", requestCount);
-
-    for (const auto &request : settings.requests) {
-      const auto ratio = request.second.counting / simulation.GetRequestCount();
-
-      const auto gos = request.second.blocking / simulation.GetRequestCount();
-
-      const auto normalized_load =
-          settings.arrivalRate *
-          (static_cast<double>(request.second.FSUs) / settings.FSUsPerLink);
-
-      document.append("requests for {} FSU(s)\n", request.second.FSUs)
-          .append("ratio: {:.3f}\n", ratio)
-          .append("grade of service: {:.3f}\n", gos)
-          .append("normalized load: {:.3f}\n", normalized_load);
-    }
-
-    const std::string report_filename = "report.txt";
-
-    document.write(report_filename);
-
-    std::cout << std::format("Simulation results wrote in {}\n",
-                             report_filename);
-
   } catch (const std::exception &exception) {
     std::cerr << exception.what() << std::endl;
 
