fcc9b2545375094e902bfd21fa22ab99b00b1039
AssoDePicche <samuelprado730@gmail.com>
2026-02-27 22:31:23 -0300

    feat: gui setup

---
+++ gui/src/app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.body);
+
+root.render(
+  <>
+  </>
+);
---
+++ gui/src/main.ts
@@ -0,0 +1,45 @@
+import { app, BrowserWindow, Menu } from 'electron';
+
+import path from 'node:path';
+
+import started from 'electron-squirrel-startup';
+
+if (started) {
+  app.quit();
+}
+
+const createWindow = () => {
+  Menu.setApplicationMenu(null);
+
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
+    );
+  }
+
+  mainWindow.webContents.openDevTools();
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
