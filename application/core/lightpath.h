641de2151c7980884397f6eac00c31afdbc11c47
AssoDePicche <samuelprado730@gmail.com>
2026-07-08 14:38:48 -0300

    feat: handle SBVT's allocations as Lightpath

--- application/core/lightpath.h
+++ application/core/lightpath.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include <cstdint>
+#include <vector>
 
 namespace core {
-struct Lightpath final {
-  uint64_t pathIndex;
+struct Allocation {
+  uint64_t fiberIndex;
   uint64_t coreIndex;
   uint64_t start;
   uint64_t end;
 };
+
+using Lightpath = std::vector<Allocation>;
 }  // namespace core
