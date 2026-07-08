bf4d7af7ccda0d84924ad25b24677debda009678
AssoDePicche <samuelprado730@gmail.com>
2026-07-08 17:47:19 -0300

    feat: update cli and visualization pipeline

--- cli/run.sh
+++ cli/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-docker build -t graph-sim .
+MIN_LOAD=${1-0}
+MAX_LOAD=${2-90}
+OFFSET=${3-10}
+CONFIG=${4-"configuration.json"}
 
-docker run --rm -v $(pwd):/app graph-sim 10 100 10
+docker run --rm -v $(pwd):/app graph-sim "$MIN_LOAD" "$MAX_LOAD" "$OFFSET" "$CONFIG"
