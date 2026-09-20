218140b0edd570a9bb8817139c55ca0223e7278e
AssoDePicche <samuelprado730@gmail.com>
2026-09-20 15:10:10 -0300

    cleanup project files and add some build optimizations

--- gui/src/adapters/preload.ts
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('api', {
-  getContainers: () => ipcRenderer.invoke('containers'),
-  onContainersUpdate: (callback: (containers: any[]) => void) => {
-    ipcRenderer.on('docker-containers-updated', (_event, value) => callback(value));
-  },
-});
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -euo pipefail
 
 BUILD_TYPE=${1:-Release}
 
+export DOCKER_BUILDKIT=1
+
 docker build \
   -f application/Dockerfile \
-  --build-arg BUILD_TYPE=$BUILD_TYPE \
+  --build-arg BUILD_TYPE="$BUILD_TYPE" \
   --build-arg BUILDKIT_INLINE_CACHE=1 \
   --target exporter \
   --output type=local,dest=. \
